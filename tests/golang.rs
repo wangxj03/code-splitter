edbc7ee1537c3de3b4d4b6e862dbf1afbafd57df
Xiaojing Wang <2002434+wangxj03@users.noreply.github.com>
2024-06-23 17:15:32 -0700

    Add integration tests for splitting Go code

---
+++ tests/golang.rs
@@ -0,0 +1,57 @@
+use code_splitter::{CharCounter, Splitter, WordCounter};
+use std::fs;
+
+#[test]
+fn split_by_chars() {
+    let code = fs::read("tests/testdata/greetings.go").unwrap();
+    let lang = tree_sitter_go::language();
+
+    let splitter = Splitter::new(lang, CharCounter).unwrap().with_max_size(200);
+    let chunks = splitter.split(&code).unwrap();
+    for (i, chunk) in chunks.iter().enumerate() {
+        println!("{i} {chunk}\n");
+    }
+}
+
+#[test]
+fn split_by_tokens_huggingface() {
+    use tokenizers::tokenizer::Tokenizer;
+
+    let code = fs::read("tests/testdata/greetings.go").unwrap();
+    let lang = tree_sitter_go::language();
+
+    let tokenizer = Tokenizer::from_pretrained("bert-base-cased", None).unwrap();
+
+    let splitter = Splitter::new(lang, tokenizer).unwrap().with_max_size(100);
+    let chunks = splitter.split(&code).unwrap();
+    for (i, chunk) in chunks.iter().enumerate() {
+        println!("{i} {chunk}\n");
+    }
+}
+
+#[test]
+fn split_by_tokens_tiktoken() {
+    use tiktoken_rs::cl100k_base;
+
+    let code = fs::read("tests/testdata/greetings.go").unwrap();
+    let lang = tree_sitter_go::language();
+    let bpe = cl100k_base().unwrap();
+
+    let splitter = Splitter::new(lang, bpe).unwrap().with_max_size(100);
+    let chunks = splitter.split(&code).unwrap();
+    for (i, chunk) in chunks.iter().enumerate() {
+        println!("{i} {chunk}\n");
+    }
+}
+
+#[test]
+fn split_by_words() {
+    let code = fs::read("tests/testdata/greetings.go").unwrap();
+    let lang = tree_sitter_go::language();
+
+    let splitter = Splitter::new(lang, WordCounter).unwrap().with_max_size(50);
+    let chunks = splitter.split(&code).unwrap();
+    for (i, chunk) in chunks.iter().enumerate() {
+        println!("{i} {chunk}\n");
+    }
+}
