13cdfa4b1d2154503851d9c94a274b43275bde47
Xiaojing Wang <2002434+wangxj03@users.noreply.github.com>
2024-08-25 23:21:43 -0700

    Add HuggingfaceSplitter class to Python bindings

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -5,7 +5,7 @@ mod language;
 pub use language::Language;
 
 mod splitter;
-pub use splitter::{CharSplitter, TiktokenSplitter, WordSplitter};
+pub use splitter::{CharSplitter, HuggingfaceSplitter, TiktokenSplitter, WordSplitter};
 
 use pyo3::prelude::*;
 
@@ -14,6 +14,7 @@ fn code_splitter(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<Chunk>()?;
     m.add_class::<Language>()?;
     m.add_class::<CharSplitter>()?;
+    m.add_class::<HuggingfaceSplitter>()?;
     m.add_class::<TiktokenSplitter>()?;
     m.add_class::<WordSplitter>()?;
     Ok(())
--- bindings/python/src/splitter.rs
+++ bindings/python/src/splitter.rs
@@ -1,12 +1,11 @@
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
 use tiktoken_rs::{cl100k_base, CoreBPE};
+use tokenizers::Tokenizer;
 
 use crate::chunk::Chunk;
 use crate::language::Language;
 use ::code_splitter::{CharCounter, Sizer, Splitter, WordCounter};
-
-const DEFAULT_MAX_SIZE: usize = 256;
 
 struct GenericSplitter<T: Sizer> {
     splitter: Splitter<T>,
@@ -52,7 +51,7 @@ pub struct CharSplitter(GenericSplitter<CharCounter>);
 #[pymethods]
 impl CharSplitter {
     #[new]
-    #[pyo3(signature = (language, max_size = DEFAULT_MAX_SIZE))]
+    #[pyo3(signature = (language, max_size))]
     fn new(language: Language, max_size: usize) -> PyResult<Self> {
         let splitter = GenericSplitter::new(language, max_size, CharCounter)?;
         Ok(CharSplitter(splitter))
@@ -69,7 +68,7 @@ pub struct WordSplitter(GenericSplitter<WordCounter>);
 #[pymethods]
 impl WordSplitter {
     #[new]
-    #[pyo3(signature = (language, max_size = DEFAULT_MAX_SIZE))]
+    #[pyo3(signature = (language, max_size))]
     fn new(language: Language, max_size: usize) -> PyResult<Self> {
         let splitter = GenericSplitter::new(language, max_size, WordCounter)?;
         Ok(WordSplitter(splitter))
@@ -86,12 +85,35 @@ pub struct TiktokenSplitter(GenericSplitter<CoreBPE>);
 #[pymethods]
 impl TiktokenSplitter {
     #[new]
-    #[pyo3(signature = (language, max_size = DEFAULT_MAX_SIZE))]
+    #[pyo3(signature = (language, max_size))]
     fn new(language: Language, max_size: usize) -> PyResult<Self> {
         let bpe = cl100k_base()
             .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))?;
         let splitter = GenericSplitter::new(language, max_size, bpe)?;
         Ok(TiktokenSplitter(splitter))
+    }
+
+    fn split(&self, code: &Bound<'_, PyBytes>) -> PyResult<Vec<PyObject>> {
+        self.0.split(code)
+    }
+}
+
+#[pyclass]
+pub struct HuggingfaceSplitter(GenericSplitter<Tokenizer>);
+
+#[pymethods]
+impl HuggingfaceSplitter {
+    #[new]
+    #[pyo3(signature = (language, max_size, pretrained_model_name_or_path))]
+    fn new(
+        language: Language,
+        max_size: usize,
+        pretrained_model_name_or_path: &str,
+    ) -> PyResult<Self> {
+        let tokenizer = Tokenizer::from_pretrained(pretrained_model_name_or_path, None)
+            .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))?;
+        let splitter = GenericSplitter::new(language, max_size, tokenizer)?;
+        Ok(HuggingfaceSplitter(splitter))
     }
 
     fn split(&self, code: &Bound<'_, PyBytes>) -> PyResult<Vec<PyObject>> {
