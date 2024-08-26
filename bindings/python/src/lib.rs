090177d1ffdd76aa92aa84e3ab498c1ca065456b
Xiaojing Wang <2002434+wangxj03@users.noreply.github.com>
2024-08-25 22:53:25 -0700

    Update Language enum and add Language class to Python bindings

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -2,13 +2,14 @@ mod chunk;
 pub use chunk::Chunk;
 
 mod splitter;
-pub use splitter::{CharSplitter, WordSplitter};
+pub use splitter::{CharSplitter, Language, WordSplitter};
 
 use pyo3::prelude::*;
 
 #[pymodule]
 fn code_splitter(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<Chunk>()?;
+    m.add_class::<Language>()?;
     m.add_class::<CharSplitter>()?;
     m.add_class::<WordSplitter>()?;
     Ok(())
--- bindings/python/src/splitter.rs
+++ bindings/python/src/splitter.rs
@@ -1,27 +1,29 @@
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
-use tree_sitter::Language;
+use tree_sitter::Language as TreeSitterLanguage;
 
 use crate::chunk::Chunk;
 use ::code_splitter::{CharCounter, Sizer, Splitter, WordCounter};
 
 const DEFAULT_MAX_SIZE: usize = 512;
 
-#[derive(Clone, Copy)]
-enum SupportedLanguage {
+#[pyclass]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[non_exhaustive]
+pub enum Language {
     Golang,
     Markdown,
     Python,
     Rust,
 }
 
-impl SupportedLanguage {
-    fn as_language(&self) -> Language {
+impl Language {
+    fn as_tree_sitter_language(&self) -> TreeSitterLanguage {
         match self {
-            SupportedLanguage::Golang => tree_sitter_go::language(),
-            SupportedLanguage::Markdown => tree_sitter_md::language(),
-            SupportedLanguage::Python => tree_sitter_python::language(),
-            SupportedLanguage::Rust => tree_sitter_rust::language(),
+            Language::Golang => tree_sitter_go::language(),
+            Language::Markdown => tree_sitter_md::language(),
+            Language::Python => tree_sitter_python::language(),
+            Language::Rust => tree_sitter_rust::language(),
         }
     }
 }
@@ -31,20 +33,8 @@ struct GenericSplitter<T: Sizer> {
 }
 
 impl<T: Sizer> GenericSplitter<T> {
-    fn new(language: &str, max_size: usize, sizer: T) -> PyResult<Self> {
-        let supported_language = match language.to_lowercase().as_str() {
-            "golang" => SupportedLanguage::Golang,
-            "markdown" => SupportedLanguage::Markdown,
-            "python" => SupportedLanguage::Python,
-            "rust" => SupportedLanguage::Rust,
-            _ => {
-                return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
-                    "Unsupported language",
-                ))
-            }
-        };
-
-        let splitter = Splitter::new(supported_language.as_language(), sizer)
+    fn new(language: Language, max_size: usize, sizer: T) -> PyResult<Self> {
+        let splitter = Splitter::new(language.as_tree_sitter_language(), sizer)
             .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))?
             .with_max_size(max_size);
 
@@ -83,7 +73,7 @@ pub struct CharSplitter(GenericSplitter<CharCounter>);
 impl CharSplitter {
     #[new]
     #[pyo3(signature = (language, max_size = DEFAULT_MAX_SIZE))]
-    fn new(language: &str, max_size: usize) -> PyResult<Self> {
+    fn new(language: Language, max_size: usize) -> PyResult<Self> {
         let splitter = GenericSplitter::new(language, max_size, CharCounter)?;
         Ok(CharSplitter(splitter))
     }
@@ -100,7 +90,7 @@ pub struct WordSplitter(GenericSplitter<WordCounter>);
 impl WordSplitter {
     #[new]
     #[pyo3(signature = (language, max_size = DEFAULT_MAX_SIZE))]
-    fn new(language: &str, max_size: usize) -> PyResult<Self> {
+    fn new(language: Language, max_size: usize) -> PyResult<Self> {
         let splitter = GenericSplitter::new(language, max_size, WordCounter)?;
         Ok(WordSplitter(splitter))
     }
